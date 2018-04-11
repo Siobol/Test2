4eec6a132cc6d66a8dbc0b722a9cda16995fa020
Siobol <norbisob@gmail.com>
2018-04-11 22:12:18 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // this is my first hello world program
+    /* this is my
+    this is my first
+    this is my first
+     */
+        System.out.print("Hello World!");
+    }
+}
